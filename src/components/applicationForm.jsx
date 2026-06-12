8badd48ae3cc0426a4fc41ddc446989dbf2299e4
djpoppa <jpoppa@colostate.edu>
2026-06-12 05:14:47 -0600

    Add application input form with company position and status fields

---
+++ src/components/applicationForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import './applicationForm.css'
+
+function ApplicationForm({ onClose, onSubmit }) {
+  const [formData, setFormData] = useState({
+  company: "",
+  position: "",
+  status: "Applied"
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(formData);
+
+    onSubmit(formData);
+    onClose;
+  }
+
+  function handleChange(e) {
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    });
+  }
+    
+  return (
+    <div 
+        className="modal-overlay"
+        onClick={onClose}
+    >
+      <div 
+        className="modal"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2>Add Application</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Company"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+          />
+
+          <input
+            type="text"
+            placeholder="Position"
+            name="position"
+            value={formData.position}
+            onChange={handleChange}
+          />
+
+          <select
+            name="status"
+            value={formData.status}
+            onChange={handleChange}
+          >
+            <option>Applied</option>
+            <option>Interview</option>
+            <option>Rejected</option>
+            <option>Offer</option>
+          </select>
+
+          <button type="submit">
+            Save
+          </button>
+
+          <button
+            type="button"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ApplicationForm;
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import ApplicationForm from "../components/applicationForm.jsx";
+
 function Jobs() {
-  return <h1>Jobs Page</h1>;
+  const [showForm, setShowForm] = useState(false);
+  const [applications, setApplications] = useState([]);
+
+  function addApplication(newApplication) {
+    setApplications([
+      ...applications,
+      newApplication
+    ]);
+  }
+  
+
+  return (
+    <>
+      <h1>Applications</h1>
+      <button
+        onClick={() =>
+          setShowForm(!showForm)
+        }
+      >
+        Add Application
+      </button>
+
+      {showForm && 
+        <ApplicationForm 
+          onClose={() => setShowForm(false)} 
+          onSubmit={addApplication}
+        />
+      }
+    </>
+    );
 }
 
 export default Jobs;
